e7a9c3d4d6d72ebbba1cf162304fc7e871fea07e
Arturo ED <xcorpio789@gmail.com>
2024-10-24 00:23:12 -0700

    diseno de formArboles

--- arbolbi/EDDProy/Estructuras No Lineales/frmArboles.Designer.cs
+++ arbolbi/EDDProy/Estructuras No Lineales/frmArboles.Designer.cs
@@ -33,21 +33,31 @@ namespace EDDemo.Estructuras_No_Lineales
             this.btnAgregar = new System.Windows.Forms.Button();
             this.txtArbol = new System.Windows.Forms.TextBox();
             this.btnLimpiar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtDato
             // 
-            this.txtDato.Location = new System.Drawing.Point(85, 30);
+            this.txtDato.Location = new System.Drawing.Point(408, 367);
+            this.txtDato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDato.Multiline = true;
             this.txtDato.Name = "txtDato";
-            this.txtDato.Size = new System.Drawing.Size(100, 46);
+            this.txtDato.Size = new System.Drawing.Size(125, 43);
             this.txtDato.TabIndex = 0;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(207, 30);
+            this.btnAgregar.Location = new System.Drawing.Point(561, 367);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(124, 46);
+            this.btnAgregar.Size = new System.Drawing.Size(113, 43);
             this.btnAgregar.TabIndex = 1;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -57,32 +67,115 @@ namespace EDDemo.Estructuras_No_Lineales
             // 
             this.txtArbol.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtArbol.Location = new System.Drawing.Point(12, 184);
+            this.txtArbol.Location = new System.Drawing.Point(191, 11);
+            this.txtArbol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtArbol.Multiline = true;
             this.txtArbol.Name = "txtArbol";
             this.txtArbol.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.txtArbol.Size = new System.Drawing.Size(776, 254);
+            this.txtArbol.Size = new System.Drawing.Size(589, 321);
             this.txtArbol.TabIndex = 2;
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(207, 91);
+            this.btnLimpiar.Location = new System.Drawing.Point(52, 32);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(124, 46);
+            this.btnLimpiar.Size = new System.Drawing.Size(93, 29);
             this.btnLimpiar.TabIndex = 1;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(52, 281);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 32);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(52, 333);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 32);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(52, 378);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 32);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(52, 428);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(93, 32);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
             // frmArboles
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(791, 483);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtArbol);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.txtDato);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmArboles";
             this.Text = "frmArboles";
             this.ResumeLayout(false);
@@ -96,5 +189,13 @@ namespace EDDemo.Estructuras_No_Lineales
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.TextBox txtArbol;
         private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
